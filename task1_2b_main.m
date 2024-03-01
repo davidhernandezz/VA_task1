ec164af367d76c31a43e4f19df7bac5291882cca
masca01 <valeri.masca@gmail.com>
2024-03-01 16:36:50 +0100

    Update task1_2b_main.m

--- task1_2b_main.m
+++ task1_2b_main.m
@@ -51,12 +51,68 @@ u(in_d, 1) = u_d;
 
 u = transpose(reshape(u, [dofs, length(K)/dofs]));
 
+% F_d = K_dd*u_d + K_dn*u_n;
+% 
+% F(in_n) = F_n_ext;
+% F(in_d) = F_d + F_d_ext;
+% 
+% F = transpose(reshape(F, [dofs, length(K)/dofs]));
+
+% new shims dimensions
+new_dimensions = u(n_supports, 2);
+
+%% Preliminary computations
+% fix_nodes matrix
+fix_nod = zeros(dofs * length(n_supports),3);
+
+for i = 1:length(n_supports)
+
+    fix_nod((dofs*i-(dofs-1):dofs*i),1) = n_supports(i);
+    fix_nod((dofs*i-(dofs-1):dofs*i),2) = 1:dofs;
+    fix_nod((dofs*i-(dofs-1):dofs*i),3) = 0;
+
+end
+
+fix_nod(2:6:32,3) = new_dimensions;
+
+% Dirichelt index vector
+in_d = (fix_nod(:, 1) - 1) * dofs + fix_nod(:, 2);
+% Dirichelt displacements vetor
+u_d = fix_nod(:, 3);
+
+in_n = setdiff(transpose(1:length(K)), in_d);
+
+g_vect = repmat(g, length(M)/dofs, 1);
+
+Fext = M * g_vect;
+
+F_n_ext = Fext(in_n);
+F_d_ext = Fext(in_d);
+
+K_nn = K(in_n, in_n);
+K_dd = K(in_d, in_d);
+K_nd = K(in_n, in_d);
+K_dn = K(in_d, in_n);
+
+% displacements and forces vectors
+u_n = K_nn\(F_n_ext - K_nd * u_d);
+
+u = zeros(length(K),1);
+
+u(in_n, 1) = u_n;
+u(in_d, 1) = u_d;
+
+u = transpose(reshape(u, [dofs, length(K)/dofs]));
+
 F_d = K_dd*u_d + K_dn*u_n;
+
+F = zeros(length(K),1);
 
 F(in_n) = F_n_ext;
 F(in_d) = F_d + F_d_ext;
-
 F = transpose(reshape(F, [dofs, length(K)/dofs]));
+
+F_d = transpose(reshape(F_d, [dofs, length(F_d)/dofs]));
 
 resultu = u(ref_node, :);
 resultF = F(ref_node, :);
@@ -64,8 +120,7 @@ resultF = F(ref_node, :);
 disp(resultu);
 disp(resultF);
 
-% new shims dimensions
-new_dimensions = 1 - u(n_supports(:, 1), 2);
+new_dimensions = 1 + u(n_supports, 2);
 
 
 
