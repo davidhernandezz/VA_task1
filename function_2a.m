38df834baa7e11b3b0b77f13467457b5cbdc328d
masca01 <valeri.masca@gmail.com>
2024-03-03 23:23:45 +0100

    2b working properly

---
+++ function_2a.m
@@ -0,0 +1,69 @@
+function resultu = function_2a(dofs)
+
+% load K & M matrix
+load('fe_model.mat');
+
+% define the nodes where supports are located
+n_supports = [10735; 13699; 16620; 19625; 22511; 4747];
+% define the referecne node
+ref_node = 1305;
+
+y_displ_support = 1; %unit displacement
+
+resultu = zeros(length(n_supports),dofs);
+resultF = zeros(length(n_supports),dofs);
+
+for displ_supp = 1:length(n_supports) %computes the reference point displacement for a given displacement in the y axis of each of the supports
+
+    %% CALCULATIONS
+
+    fix_nod = fixnodes_2a(n_supports, dofs, displ_supp, y_displ_support);
+
+    % Dirichelt index vector
+    in_d = (fix_nod(:, 1) - 1) * dofs + fix_nod(:, 2);
+    % Dirichelt displacements vetor
+    u_d = fix_nod(:, 3);
+
+    in_n = setdiff(transpose(1:length(K)), in_d);
+
+    % gravity acceleration vector
+    g = [0; 9.81 * 10^3; 0; 0; 0; 0];
+
+    g_vect = repmat(g, length(K)/dofs, 1);
+
+    Fext = M * g_vect;
+
+    F_n_ext = Fext(in_n);
+    F_d_ext = Fext(in_d);
+
+    K_nn = K(in_n, in_n);
+    K_dd = K(in_d, in_d);
+    K_nd = K(in_n, in_d);
+    K_dn = K(in_d, in_n);
+
+    % displacements and forces vectors
+    u_n = K_nn\(F_n_ext - K_nd * u_d);
+
+    u = zeros(length(K),1);
+
+    u(in_n, 1) = u_n;
+    u(in_d, 1) = u_d;
+
+    u = transpose(reshape(u, [dofs, length(K)/dofs]));
+
+    F_d = K_dd*u_d + K_dn*u_n;
+
+    F = zeros(length(K),1);
+
+    F(in_n) = F_n_ext;
+    F(in_d) = F_d + F_d_ext;
+    F = transpose(reshape(F, [dofs, length(K)/dofs]));
+
+
+    % displacements of the reference node
+    resultu(displ_supp,:) = u(ref_node, :);
+
+    % reaction forces of the reference node
+    resultF(displ_supp,:) = F(ref_node, :);
+
+end
--- task1_2b_main.m
+++ task1_2b_main.m
@@ -19,7 +19,14 @@ z_rot = 200*10^-6;
 
 %% CALCULATIONS
 
-fix_nod = fixnodes_2b(n_supports, ref_node, dofs, x_rot, z_rot);
+resultu = transpose(function_2a(dofs));
+
+B = [0; 0; 0; -x_rot; 0; -z_rot];
+
+% new shims dimensions
+new_dimensions = resultu\B;
+
+fix_nod = fixnodes_2c(n_supports, dofs, new_dimensions);
 
 % Dirichelt index vector
 in_d = (fix_nod(:, 1) - 1) * dofs + fix_nod(:, 2);
@@ -32,63 +39,6 @@ in_n = setdiff(transpose(1:length(K)), in_d);
 g = [0; 0; 0; 0; 0; 0];
 
 g_vect = repmat(g, length(K)/dofs, 1);
-
-Fext = M * g_vect;
-
-F_n_ext = Fext(in_n);
-F_d_ext = Fext(in_d);
-
-K_nn = K(in_n, in_n);
-K_dd = K(in_d, in_d);
-K_nd = K(in_n, in_d);
-K_dn = K(in_d, in_n);
-
-% displacements and forces vectors
-u_n = K_nn\(F_n_ext - K_nd * u_d);
-
-u(in_n, 1) = u_n;
-u(in_d, 1) = u_d;
-
-u = transpose(reshape(u, [dofs, length(K)/dofs]));
-
-F_d = K_dd*u_d + K_dn*u_n;
-
-F(in_n) = F_n_ext;
-F(in_d) = F_d + F_d_ext;
-
-F = transpose(reshape(F, [dofs, length(K)/dofs]));
-
-resultu = u(ref_node, :);
-resultF = F(ref_node, :);
-
-disp(resultu);
-disp(resultF);
-
-% new shims dimensions
-new_dimensions = u(n_supports, 2);
-
-%% Preliminary computations
-% fix_nodes matrix
-fix_nod = zeros(dofs * length(n_supports),3);
-
-for i = 1:length(n_supports)
-
-    fix_nod((dofs*i-(dofs-1):dofs*i),1) = n_supports(i);
-    fix_nod((dofs*i-(dofs-1):dofs*i),2) = 1:dofs;
-    fix_nod((dofs*i-(dofs-1):dofs*i),3) = 0;
-
-end
-
-fix_nod(2:6:32,3) = new_dimensions;
-
-% Dirichelt index vector
-in_d = (fix_nod(:, 1) - 1) * dofs + fix_nod(:, 2);
-% Dirichelt displacements vetor
-u_d = fix_nod(:, 3);
-
-in_n = setdiff(transpose(1:length(K)), in_d);
-
-g_vect = repmat(g, length(M)/dofs, 1);
 
 Fext = M * g_vect;
 
@@ -118,8 +68,6 @@ F(in_n) = F_n_ext;
 F(in_d) = F_d + F_d_ext;
 F = transpose(reshape(F, [dofs, length(K)/dofs]));
 
-F_d = transpose(reshape(F_d, [dofs, length(F_d)/dofs]));
-
 resultu = u(ref_node, :);
 resultF = F(ref_node, :);
 
@@ -127,6 +75,4 @@ disp(resultu);
 disp(resultF);
 
 new_dimensions = 1 + u(n_supports, 2);
-
-
 
